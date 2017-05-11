1697edb41819621ca872ae72e5bc2834c9465c28
Tobias Arweiler <t.arweiler@nutmountain.io>
2017-05-11 09:35:21 +0200

    included sass loader and added some more structure

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,26 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  entry: './main.js',
+  entry: './src/main.js',
   output: { path: __dirname + '/public', filename: 'bundle.js' },
   module: {
     loaders: [
       {
-        test: /.jsx?$/,
+        test: /.js?$/,
         loader: 'babel-loader',
+        include: path.join(__dirname, 'src'),
         exclude: /node_modules/,
         query: {
           presets: ['es2015', 'react']
         }
+      },
+      {
+        test: /\.scss$/,
+        loaders: [
+            'style-loader',
+            'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
+            'sass-loader'
+        ]
       }
     ]
   },
