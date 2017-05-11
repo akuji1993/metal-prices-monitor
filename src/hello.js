2f5bb759094608ac891e423c0fb8ba66046ed29f
Tobias Arweiler <t.arweiler@nutmountain.io>
2017-05-11 09:36:24 +0200

    included sass loader and added some more structure

---
+++ src/hello.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import s from './hello.scss';
+
+class Hello extends React.Component {
+
+  componentWillMount() {
+    let quandlApiKey = "ZRSDJExtCK25PExCpxsy";
+
+    let copperJson = [];
+    let zincJson = [];
+
+    let copperUrl = "LME/PR_CO";
+    let zincUrl = "LME/PR_ZI";
+
+    let quandlUrl = "https://www.quandl.com/api/v3/datasets"
+    let apiUrl = ("?api_key=" + quandlApiKey);
+    let dataUrl = "data.json";
+
+    let copUrl = (quandlUrl + "/" + copperUrl + "/" + dataUrl + apiUrl);
+    console.log(copUrl);
+
+    let ziUrl = (quandlUrl + "/" + zincUrl + "/" + dataUrl + apiUrl);
+    console.log(ziUrl);
+
+    fetch(copUrl).then(function(response) {
+        return response.json();
+    }).then(function(blob){
+      console.log(blob);
+    });
+
+    fetch(ziUrl).then(function(response) {
+        return response.json();
+    }).then(function(blob){
+      console.log(blob);
+    });
+  }
+
+  render() {
+
+    return <div className={s.test}>Text</div>
+  }
+}
+
+ReactDOM.render(<Hello/>, document.getElementById('hello'));
